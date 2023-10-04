ddf1dd478c4ad75379f5d9c895811cfb8ec20a46
imchopi <apchopito@gmail.com>
2023-10-04 09:38:48 +0200

    Added component properties

--- src/app/home/user-info/user-info.component.ts
+++ src/app/home/user-info/user-info.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss'],
 })
-export class UserInfoComponent  implements OnInit {
+export class UserInfoComponent implements OnInit {
+  private _name: string = '';
+  private _surname: string = '';
+  private _age: number = 0;
 
-  constructor() { }
+  constructor() {}
+
+  @Input() 
+  set name(new_name: string) {this._name = new_name}
+  get name(): string {return this._name}
+
+  @Input() 
+  set surname(new_surname: string) {this._surname = new_surname}
+  get surname(): string {return this._surname}
+
+  @Input() 
+  set age(new_age: number) {this._age = new_age}
+  get age(): number {return this._age}
 
   ngOnInit() {}
-
 }
